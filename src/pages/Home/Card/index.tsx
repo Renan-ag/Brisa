2f415d3453e6bce829cad69997029224b82ea1b8
renan-ag <ag.renan@hotmail.com>
2025-03-25 10:16:39 -0300

    Creating text overflow style

--- src/pages/Home/Card/index.tsx
+++ src/pages/Home/Card/index.tsx
@@ -16,15 +16,19 @@ const Card = ({ content }: { content: IPost }) => {
             <img src={content.imageUrl} alt="Imagem de setup tematico do Raichu (Pokemon)" />
           </Link>
         </div>
-        <div className="px-16 mt-16">
-          <div className="flex-between ">
+        <div className="px-16 mt-16 h-50">
+          <div className="flex-between">
             <h6 className="uppercase color-gray">{content.category}</h6>
             <h6 className="color-gray">{formatDateToPost(content.date)}</h6>
           </div>
-          <h4 className="fw-normal mt-8">{content.title}</h4>
-          <p className="mt-8">{content.resume}</p>
-          <div className="flex-center my-16">
-            <Link to={`/post/${content.id}`} className="link color-blue"> Ler mais </Link>
+          <div className='flex-start flex-column h-100'>
+            <h4 className="fw-normal mt-8">{content.title}</h4>
+            <div className='flex-grow-1'>
+              <p className="mt-8">{content.resume}</p>
+            </div>
+            <div className="flex-center w-100 pb-8">
+              <Link to={`/post/${content.id}`} className="link color-blue"> Ler mais </Link>
+            </div>
           </div>
         </div>
       </div>
