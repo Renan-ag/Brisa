62d172695bc7622550013c9d62544f050099b299
renan-ag <ag.renan@hotmail.com>
2025-03-26 16:08:53 -0300

    Removing particles in mobile to better performance

--- src/pages/Home/Hero/index.tsx
+++ src/pages/Home/Hero/index.tsx
@@ -5,14 +5,17 @@ const Hero = () => {
     <>
       <section className="container wind-start position-relative hero">
         <div className="row">
-          <div className="col-6 pl-72">
-            <h1 className="brand-font fw-normal h0">Brisa</h1>
-            <p className=" mt-16">
-              Brisa é onde as ideias sopram livremente. Com conteúdos diversos, de notícias a reflexões e análises, nosso
-              objetivo é trazer um ar novo de inspiração e conhecimento para sua rotina.
-            </p>
+          <div className="col-6 flex-start">
+            <div className="pl-72 remove-in-sm"></div>
+            <div>
+              <h1 className="brand-font fw-normal h0">Brisa</h1>
+              <p className=" mt-16">
+                Brisa é onde as ideias sopram livremente. Com conteúdos diversos, de notícias a reflexões e análises, nosso
+                objetivo é trazer um ar novo de inspiração e conhecimento para sua rotina.
+              </p>
+            </div>
           </div>
-          <div className="col-6 h-100">
+          <div className="col-6 h-100 remove-in-sm">
             <WindParticle />
           </div>
         </div>
