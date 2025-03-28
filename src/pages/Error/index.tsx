fdec8555814de6189be0ef474675b86e0cf46faa
renan-ag <ag.renan@hotmail.com>
2025-03-27 23:57:44 -0300

    Changing error page style

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div className="container flex-center flex-column flex-grow-1">
-      <h2 className="color-red text-center">Ocorreu um erro ao acessar o servidor ⚠</h2>
+    <div className="container text-container flex-center flex-column flex-grow-1">
+      <h3 className="color-red fw-normal text-center">Ocorreu um erro ao acessar o servidor ⚠</h3>
       <div className="mt-16">
-        <h5 className="color-white fw-normal">Parece que houve um erro com nosso servidor mas já estamos consertando 🔧</h5>
+        <h5 className="color-gray text-center fw-normal">Parece que houve um erro com nosso servidor mas já estamos consertando 🔧</h5>
         <Link to="/" className="btn w-100 text-center mt-32">Tentar accessar novamente</Link>
       </div>
     </div>
