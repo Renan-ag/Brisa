47c321d43562e9b8ae4e9e2a8dd26d1ce4f95c93
renan-ag <ag.renan@hotmail.com>
2025-03-27 23:57:24 -0300

    Indentation

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -5,9 +5,12 @@ const About = () => {
         <div>
           <h2 className="text-center fw-normal">Sobre</h2>
           <p className="mt-16 color-white">
-            Bem-vindo ao <b>Brisa</b>, um blog criado para inspirar, informar e conectar pessoas através de histórias, reflexões e experiências. Aqui, exploramos temas como viagens, cultura, estilo de vida e sustentabilidade, sempre com um toque de autenticidade e paixão. <br /> <br />
-            Acreditamos que cada palavra carrega uma brisa de mudança, capaz de renovar perspectivas e despertar novas ideias. Nosso objetivo é criar um espaço acolhedor, onde todos se sintam parte de uma comunidade que valoriza a criatividade e o conhecimento compartilhado. <br /> <br />
-
+            Bem-vindo ao <b>Brisa</b>, um blog criado para inspirar, informar e conectar pessoas através de histórias, reflexões e experiências.
+            Aqui, exploramos temas como viagens, cultura, estilo de vida e sustentabilidade, sempre com um toque de autenticidade e paixão.
+            <br /><br />
+            Acreditamos que cada palavra carrega uma brisa de mudança, capaz de renovar perspectivas e despertar novas ideias. Nosso objetivo é
+            criar um espaço acolhedor, onde todos se sintam parte de uma comunidade que valoriza a criatividade e o conhecimento compartilhado.
+            <br /><br />
             <i>Junte-se a nós nessa jornada e deixe-se levar pelo vento suave do Brisa.</i>
           </p>
         </div>
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -6,8 +6,10 @@ const Contact = () => {
         <div className="row mt-8">
           <div className="col-6 py-0">
             <p className="text-justify color-white">
-              Seja bem-vindo(a) ao Brisa, um espaço dedicado a compartilhar ideias, inspirações e reflexões sobre vida, viagens, cultura e tudo o que nos move. Queremos ouvir você! <br /> <br />
-              Se tiver dúvidas, sugestões ou apenas quiser bater um papo, não hesite em nos contatar. Sua opinião é importante para nós e ajuda a tornar o Brisa cada vez melhor. <br /> <br />
+              Seja bem-vindo(a) ao Brisa, um espaço dedicado a compartilhar ideias, inspirações e reflexões sobre vida, viagens, cultura e tudo o que nos move. Queremos ouvir você!
+              <br /> <br />
+              Se tiver dúvidas, sugestões ou apenas quiser bater um papo, não hesite em nos contatar. Sua opinião é importante para nós e ajuda a tornar o Brisa cada vez melhor.
+              <br /> <br />
               <i>O Brisa é mais que um blog, é uma comunidade. Juntos, criamos ventos de mudança. 🌿</i>
             </p>
             <h5 className="color-white fw-normal mt-16">E-mail: Test@email.com</h5>
@@ -18,9 +20,7 @@ const Contact = () => {
               <input type="text" className="mt-16" placeholder="Email" name="email" />
               <textarea name="message" className="mt-16" placeholder="Mensagem" rows={8}></textarea>
               <div className="mt-16 flex-end">
-                <button className="btn ">
-                  Enviar
-                </button>
+                <button className="btn">Enviar</button>
               </div>
             </form>
           </div>
