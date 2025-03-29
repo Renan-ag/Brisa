3ce24c4308dfe0cf29063b767282b31b5a90180b
renan-ag <ag.renan@hotmail.com>
2025-03-29 11:38:37 -0300

    Changing page title font weight

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -2,7 +2,7 @@ const Contact = () => {
   return (
     <>
       <div className="container flex-grow-1">
-        <h2 className="ml-16">Fale com a gente</h2>
+        <h2 className="ml-16 fw-normal">Fale com a gente</h2>
         <div className="row mt-8">
           <div className="col-6 py-0">
             <p className="text-justify color-white">
