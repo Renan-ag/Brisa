09385c9861dba468fc19451938bd15e4b7f45c3e
renan-ag <ag.renan@hotmail.com>
2025-03-31 07:50:02 -0300

    Adding "Explorar" link to mobile menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,19 @@
 // Svg
-import wind from 'svg/wind.svg'
+import wind from "svg/wind.svg";
 // Styles
-import './header.css';
+import "./header.css";
 // Components / Hooks  / Types
-import { Link, useNavigate } from 'react-router-dom';
-import { FormEvent, useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import { FormEvent, useState } from "react";
 
 const Header = () => {
   const navigate = useNavigate();
   const [searchValue, setSearchValue] = useState<string>();
   const [showMobileMenu, setShowMobileMenu] = useState(false);
 
-  const handleInputChange= (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
-  }
+  };
 
   function handleSearch(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -32,45 +32,103 @@ const Header = () => {
             Brisa
           </Link>
           <ul>
-            <li><Link to="/pesquisa">Explorar</Link></li>
-            <li><Link to="/sobre">Sobre</Link></li>
-            <li><Link to="/contato">Contato</Link></li>
+            <li>
+              <Link to="/pesquisa">Explorar</Link>
+            </li>
+            <li>
+              <Link to="/sobre">Sobre</Link>
+            </li>
+            <li>
+              <Link to="/contato">Contato</Link>
+            </li>
           </ul>
         </nav>
 
-        <div onClick={toggleMobileMenu} className={`bx ${showMobileMenu && 'active'}`}></div>
+        <div
+          onClick={toggleMobileMenu}
+          className={`bx ${showMobileMenu && "active"}`}
+        ></div>
 
         <div className="flex-start">
           <div className="search">
             <form onSubmit={handleSearch} className="flex-center">
-              <input type="text" name="search" onChange={handleInputChange} value={searchValue} placeholder="Buscar..." />
-              <button type='submit' title='Pesquisar' className="btn-search"></button>
+              <input
+                type="text"
+                name="search"
+                onChange={handleInputChange}
+                value={searchValue}
+                placeholder="Buscar..."
+              />
+              <button
+                type="submit"
+                title="Pesquisar"
+                className="btn-search"
+              ></button>
             </form>
           </div>
 
           <div className="cta-desktop ml-24">
-            <Link to="/login" className="btn">Login</Link>
+            <Link to="/login" className="btn">
+              Login
+            </Link>
           </div>
 
           <div className="cta-mobile">
-            <Link to="/login" className="link bold">Login</Link>
+            <Link to="/login" className="link bold">
+              Login
+            </Link>
           </div>
         </div>
       </header>
-      <div className={`menu-mobile ${showMobileMenu && 'show-menu'}`}>
+      <div className={`menu-mobile ${showMobileMenu && "show-menu"}`}>
         <ul className="nav-mobile">
-          <li><Link className="link-menu-mobile" to="/sobre">Sobre</Link></li>
-          <li><Link className="link-menu-mobile" to="/contato">Contato</Link></li>
+          <li>
+            <Link
+              onClick={toggleMobileMenu}
+              className="link-menu-mobile"
+              to="/pesquisa"
+            >
+              Explorar
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={toggleMobileMenu}
+              className="link-menu-mobile"
+              to="/sobre"
+            >
+              Sobre
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={toggleMobileMenu}
+              className="link-menu-mobile"
+              to="/contato"
+            >
+              Contato
+            </Link>
+          </li>
           <li className="p-16">
             <form onSubmit={handleSearch} className="flex-center">
-              <input type="text" name="search" onChange={handleInputChange} value={searchValue} placeholder="Buscar..." />
-              <button type='submit' title='Pesquisar' className="btn-search"></button>
+              <input
+                type="text"
+                name="search"
+                onChange={handleInputChange}
+                value={searchValue}
+                placeholder="Buscar..."
+              />
+              <button
+                type="submit"
+                title="Pesquisar"
+                className="btn-search"
+              ></button>
             </form>
           </li>
         </ul>
       </div>
     </>
   );
-}
+};
 
 export default Header;
